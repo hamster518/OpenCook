c4f3aff3bd64a398c7ecac7d15a8d688a5ebf413
Larry_LIu <hamster518@gmail.com>
2017-07-29 23:47:17 +0800

    Move and Rename prod_settings.py

--- opencook/prod_settings.py
+++ opencook/prod_settings.py
@@ -1,10 +1,11 @@
-from .settings import * import dj_database_url
+from .settings import * 
+import dj_database_url
 # 把 debug 模式關掉 
 DEBUG = False
 # 遵守 HTTPS 連線中的 "X-Forwarded-Proto" header 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # 設定靜態檔位置 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 # 設定資料庫 
 DATABASES = {
 	'default': dj_database_url.config() 
